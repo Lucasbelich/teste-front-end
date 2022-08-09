b10bf67291434abee1399bd940733ea665005bec
Lucasbelich <lucasbelich1@gmail.com>
2022-08-09 15:02:19 -0300

    fixed-basename

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App basename="/teste-front-end"/>
   </React.StrictMode>
 );
